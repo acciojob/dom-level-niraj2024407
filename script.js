6b1b9f70ad9f51dbcad84a90311967a2218ea0d9
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-28 07:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// levelFinder.js
+document.addEventListener('DOMContentLoaded', function () {
+  var el = document.getElementById('level');
+  var level = 0;
+
+  if (el) {
+    var curr = el.parentElement;
+    while (curr) {
+      level++;
+      curr = curr.parentElement;
+    }
+  }
+
+  alert('The level of the element is: ' + level);
+});
